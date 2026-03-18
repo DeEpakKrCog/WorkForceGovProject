8458cd4e0d09c5e7790ab3cad31f0b35ab5f6198
madhubala-sys <madhubala.s@cognizant.com>
2026-03-18 21:39:08 +0530

    ProgramManagerController.cs

--- WorkForceGovProject/Controllers/ProgramManagerController.cs
+++ WorkForceGovProject/Controllers/ProgramManagerController.cs
@@ -1,20 +1,21 @@
-using System.Web.Mvc;
-using WorkForceGov.Models;
 
-namespace WorkForceGov.Controllers
+using Microsoft.AspNetCore.Mvc;
+using WorkForceGovProject.Models;
+
+namespace WorkForceGovProject.Controllers
 {
     public class ProgramManagerController : Controller
     {
-        public ActionResult Dashboard()
+        public IActionResult Dashboard()
         {
-            // Sample data (normally fetched from database)
-            ProgramManagerDashboardViewModel model = new ProgramManagerDashboardViewModel
-            {
-                TotalPrograms = 7,
-                TotalBudget = 1200000,
-                ActiveTrainingPrograms = 5,
-                PerformancePercentage = 78.5
-            };
+            ProgramManagerDashboardViewModel model =
+                new ProgramManagerDashboardViewModel
+                {
+                    TotalPrograms = 7,
+                    TotalBudget = 1200000,
+                    ActiveTrainingPrograms = 5,
+                    PerformancePercentage = 78
+                };
 
             return View(model);
         }
